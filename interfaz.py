97d776690b2eed790392fac181b70cbf7239713f
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-12-13 16:05:05 -0500

    buscar archivo y eliminando comentarios

--- interfaz.py
+++ interfaz.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import filedialog
 #raiz
 raiz= tkinter.Tk()
-raiz.title=("PROYECTO ARQUITECTURA DE COMPUTADORES")
+raiz.title=("AC")
 #cambiar el icono
 raiz.iconbitmap("podcast.ico")
 #tamaño de la raiz
@@ -22,7 +22,7 @@ miFrame.config(cursor="hand2")
 
 
 #aun no sale imagen
-miImagen=PhotoImage(file="icono2.png")
+miImagen=PhotoImage(file="icono.png")
 Label(miFrame,image=miImagen).place(x=5,y=5)
 
 #------------------------------------------------------------------
@@ -30,36 +30,23 @@ Label(miFrame,image=miImagen).place(x=5,y=5)
 #miImagen.place(x=200,y=0)
 
 
-tituloPrincipal=Label(miFrame, text="POTCAST",fg="black",font=("Comic Sans MS",18))
-tituloPrincipal.place(x=400,y=0)
+tituloPrincipal=Label(miFrame, text="PROYECTO ARQUITECTURA DE COMPUTADORES",fg="black",font=("Comic Sans MS",18))
+tituloPrincipal.place(x=100,y=0)
 
 
 #TITULO
-etiquetaTitulo=Label(miFrame, text="titulo",fg="black",font=("Comic Sans MS",12))
+etiquetaTitulo=Label(miFrame, text="Nombre del Archivo",fg="black",font=("Comic Sans MS",12))
 etiquetaTitulo.place(x=600,y=120)
 
 etiquetaBuscarArchivo=Label(miFrame, text="Abrir Archivo",fg="black",font=("Comic Sans MS",12))
-etiquetaBuscarArchivo.place(x=80,y=160)
+etiquetaBuscarArchivo.place(x=20,y=160)
 #CAJA DE TEXTO,como comentario
 cajaTexto = tkinter.Text(miFrame)
 cajaTexto.place(x=500,y=150,width=270,height=300)
 
 
-#
-#cajaTexto = tkinter.Entry(miFrame)
-#cajaTexto.place(x=500,y=150,width=270,height=300)
-
-
-
-
-#etiqueta TITULO y Posicion
-#etiqueta = tkinter.Label(raiz,text = "POSTCAST", bg="blue")
-#etiqueta.pack()
-raiz.mainloop()
-
-
-
-##def test():
+#BOTON ABRIR ARCHIVOS
+def test():
     # *Ventana de elección de color
     # color = colorchooser.askcolor(title="Elige un color")
     # print(color)
@@ -70,9 +57,13 @@ raiz.mainloop()
     # print(fichero)
     # *Equivale a open("ruta","w")
     # mode -> formato de abrir el archivo
-##   fichero = filedialog.asksaveasfile(miFrame,title="Guardar un archivo", mode="r+")
-##  if fichero is not None:
-##     fichero.write("Texto de relleno").place(x=20,y=20)
-##     #faltaria dimensionarlo
-##    fichero.close()
-##Button(raiz, text="Click", command=test).pack()
+    fichero = filedialog.asksaveasfile(title="Guardar un archivo", mode="r+")
+    if fichero is not None:
+        fichero.write("Texto")
+        fichero.close()
+
+Button(miFrame, text="Click", command=test).place(x=180,y=160)
+
+raiz.mainloop()
+
+
